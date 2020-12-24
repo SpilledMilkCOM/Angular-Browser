6b6d6b73939be4e44ff4441e401b26e4e111fb80
Parker Smart <psmart@spilledmilk.com>
2020-12-23 17:50:05 -0700

    Checkboxes wired up.

--- Angular-Browser/src/app/app.component.ts
+++ Angular-Browser/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 import { MapperService } from 'src/app-mappers/app-mapper-service'
 
@@ -8,29 +8,49 @@ import { MapperService } from 'src/app-mappers/app-mapper-service'
   styleUrls: ['./app.component.css'],
   providers: [MapperService]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  mapShout = "false";
+  public mapCompress = false;
+  public mapLeet = false;
+  public mapShout = false;
+  public mapVowels = false;
 
-  mapped = '';
-  message = '';
-  title = 'Angular-Browser';
+  public mapped = 'init default';
+  public message = 'init default';
+  public title = 'Angular-Browser';
 
   constructor(
     private mapper: MapperService
   ) {
   }
 
-  public clickShout(checked: string) {
-    console.log('clickShout(' + checked + ')');
+  public clickCompress(checked: boolean) {
+    // Seems like there is only SOME binding to the variable.  Make sure it's set here.
 
-    this.mapShout = checked;
+    this.mapCompress = checked;
+
+    this.mapMessage(this.message);
   }
 
-  public onKeyUp(event: any) {
-    // This needs to be "key up" otherwise the value of target will be the one BEFORE versus what it is AFTER the key is pressed.
+  public clickLeet(checked: boolean) {
+    // Seems like there is only SOME binding to the variable.  Make sure it's set here.
 
-    var value = event.target.value;
+    this.mapShout = checked;
+
+    this.mapMessage(this.message);
+  }
+
+  public clickShout(checked: boolean) {
+    // Seems like there is only SOME binding to the variable.  Make sure it's set here.
+
+    this.mapShout = checked;
+
+    this.mapMessage(this.message);
+  }
+
+  public mapMessage(message: string) {
+
+    var value = message;
 
     // Need to do all of the mapping here based on the mapper / translator.
 
@@ -38,6 +58,19 @@ export class AppComponent {
       value = this.mapper.map(value);
     }
 
+    console.log('mapShout = ' + this.mapShout);
+
     this.mapped = value;
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  public onKeyUp(event: any) {
+    this.message = event.target.value;
+
+    // This needs to be "key up" otherwise the value of target will be the one BEFORE versus what it is AFTER the key is pressed.
+
+    this.mapMessage(this.message);
   }
 }
